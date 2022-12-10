2506d254a6ef023da08e609557ca7d664eec184c
Jacob Kedar Krevat <jacobtie@gmail.com>
2022-12-09 19:10:43 -0500

    Trying different create db syntax

--- create_db.py
+++ create_db.py
@@ -1,8 +1,5 @@
 from app import app
 from src.models import db
 
-with open('schema.sql', 'r') as sql_file:
-    sql = sql_file.read()
-
 with app.app_context():
-    db.engine.execute(sql)
+    db.create_all()
